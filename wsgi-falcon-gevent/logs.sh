71aa2a67f12ee67b1f9b3ef16014ec2062eacad9
pablomartinfranco <49606205+pablomartinfranco@users.noreply.github.com>
2026-07-25 02:05:31 -0300

    add logs sh command

--- wsgi-falcon-gevent/logs.sh
+++ wsgi-falcon-gevent/logs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+# SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 # tail -F "$SCRIPT_DIR/web/0a/wsgi-falcon-gevent.0a.com.ar/stderr.log"
-tail -F "~/web/0a/wsgi-falcon-gevent.0a.com.ar/stderr.log"
+tail -F ~/web/0a/wsgi-falcon-gevent.0a.com.ar/stderr.log
 
 
 
