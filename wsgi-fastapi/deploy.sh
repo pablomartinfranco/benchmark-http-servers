0179e1b9b1370fb0271186ca491fb7dd0562b649
pablomartinfranco <49606205+pablomartinfranco@users.noreply.github.com>
2026-07-29 00:30:35 -0300

    add deploy wsgi-fastapi

--- wsgi-fastapi/deploy.sh
+++ wsgi-fastapi/deploy.sh
@@ -10,12 +10,15 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
+PYTHON_VERSION="3.11"
+
 APP_DIR="$(basename "$PWD")"
-VENV="$HOME/virtualenv/web/0a/$APP_DIR/3.11"
+
+VENV="$HOME/virtualenv/web/0a/$APP_DIR/$PYTHON_VERSION"
+
+rm -rf "$VENV/lib/python$PYTHON_VERSION/site-packages"
 
 source "$VENV/bin/activate"
-
-# rm -rf "$VIRTUAL_ENV/lib/python3.11/site-packages"
 
 python -m pip install --upgrade pip
 
@@ -24,20 +27,10 @@ python -m pip install --upgrade pip
 #   | cut -d= -f1 \
 #   | xargs -r python -m pip uninstall -y
 
-packages=$(
-  python -m pip list --format=freeze \
-    | grep -vE '^(pip|setuptools|wheel)==' \
-    | cut -d= -f1 || true
-)
-
-if [ -n "$packages" ]; then
-    echo "$packages" | xargs python -m pip uninstall -y
-fi
-
-# python -m pip install -r requirements.txt
 python -m pip install --upgrade -r requirements.txt
 
 mkdir -p tmp
+
 touch tmp/restart.txt
 
 echo "Deployment completed for $APP_DIR"
@@ -45,23 +38,3 @@ echo "Deployment completed for $APP_DIR"
 # Enter to the virtual environment.
 # To enter to virtual environment, run the command: 
 # source ~/virtualenv/web/0a/wsgi.0a.com.ar/3.11/bin/activate && cd ~/web/0a/wsgi.0a.com.ar
-
-
-
-# #!/usr/bin/env bash
-# set -e
-
-# cd "$(dirname "$0")"
-
-# source ~/virtualenv/web/0a/wsgi-fastapi.0a.com.ar/3.11/bin/activate
-
-# pip install --upgrade pip
-# pip list --format=freeze | grep -vE '^(pip|setuptools|wheel)==' | cut -d= -f1 | xargs -r pip uninstall -y
-# pip install -r requirements.txt
-
-# mkdir -p tmp
-# touch tmp/restart.txt
-
-# # Enter to the virtual environment.
-# # To enter to virtual environment, run the command: 
-# # source ~/virtualenv/web/0a/wsgi-falcon-gevent.0a.com.ar/3.11/bin/activate && cd ~/web/0a/wsgi-falcon-gevent.0a.com.ar
