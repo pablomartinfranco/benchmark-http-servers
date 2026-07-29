d416c4bb20d84d95f76355e2d847fe096c252009
pablomartinfranco <49606205+pablomartinfranco@users.noreply.github.com>
2026-07-28 23:21:49 -0300

    add deploy wsgi-fastapi

---
+++ wsgi-fastapi/logs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tail -F ~/web/0a/wsgi-fastapi.0a.com.ar/stderr.log
+
+# tail -F \
+#     "$SCRIPT_DIR/web/0a/wsgi-falcon-gevent.0a.com.ar/stderr.log" \
+#     "$SCRIPT_DIR/web/0a/wsgi-falcon-gevent.0a.com.ar/stdout.log"
